bc3e0f56c2152fef109e20512fcb587dee927b44
zhangwei <853014022@qq.com>
2020-10-10 14:36:48 +1100

    fix bug

--- Project_without_framework/Logistic_Regression/Mushroom_classification_logistic_regression.py
+++ Project_without_framework/Logistic_Regression/Mushroom_classification_logistic_regression.py
@@ -37,50 +37,117 @@ class LogisticRegressionModel:
         self.final_cost = None
 
     def sigmoid(self, z):
-        """sigmoid function"""
+        """
+        sigmoid function
+        :param z: linear part input
+        :return:
+        sigmoid output
+        """
         return 1 / (1 + np.exp(-z))
 
-    def model(self, x, theta):
-        """activate by sigmoid function"""
-        s1 = np.dot(x,theta)
+    def activation(self, x, theta):
+        """
+        sigmoid activation
+        :param x: feature matrix
+        :param theta: weight matrix
+        :return:
+        s: activation output
+        """
+        s1 = np.dot(x, theta)
         s = self.sigmoid(s1)
+
+        assert s.shape == (x.shape[0], 1)  # shape check
+
         return s
 
     def cost(self, x, y, theta, m):
-        """cost function"""
-        a = np.multiply(-y, np.log(self.model(x, theta)))
-        b = np.multiply(1 - y, np.log(1 - self.model(x, theta)))
-        s = np.sum(a - b) / m
+        """
+        compute cost
+        :param x: feature matrix
+        :param y: class label matrix
+        :param theta: weight matrix
+        :param m: size of data
+        :return:
+        s: cost
+        """
+        a = np.multiply(y, np.log(self.activation(x, theta)))
+        b = np.multiply(1 - y, np.log(1 - self.activation(x, theta)))
+        s = np.sum(a + b) / (-m)
+
+        assert s.shape == ()  # shape check
+
         return s
 
     def gradient(self, x, y, theta):
-        """calculate grad"""
-        grad = -(x.T.dot(y-self.model(x,theta)))
+        """
+        compute gradient
+        :param x: feature matrix
+        :param y: class label matrix
+        :param theta: weight matrix
+        :return:
+        grad: gradient
+        """
+        grad = x.T.dot(self.activation(x, theta) - y)
+
+        assert grad.shape == theta.shape  # shape check
+
         return grad
 
-    def descent(self, x, y, n, m, alpha, theta):
-        """gradient decent"""
-        i = 0
-        costs = [self.cost(x,y,theta,m)]
+    def descent(self, x, y, iteration_num, m, initial_alpha, theta, lambd, decay_rate):
+        """
+        training model by gradient descent
+        :param x: feature matrix
+        :param y: class label matrix
+        :param iteration_num: iteration number
+        :param m: size of data
+        :param initial_alpha: initial learning rate alpha
+        :param theta: weight matrix
+        :param lambd: regularization term lambda
+        :param decay_rate: decay rate of learning rate
+        :return:
+        final_theta: trained weight matrix
+        final_cost: cost of last training
+        """
+        i = 1
+        costs = [self.cost(x, y, theta, m)]
+        alpha = initial_alpha
         while True:
-            grad = self.gradient(x,y,theta)
-            theta = theta - (alpha/m) * grad
-            costs.append(self.cost(x,y,theta,m))
+            grad = self.gradient(x, y, theta)
+            theta = theta - (alpha / m) * grad + (lambd / m) * theta  # update theta
+            current_cost = self.cost(x, y, theta, m)
+            costs.append(current_cost)
+
+            # print iteration number and cost every 100 iterations
+            if i % 100 == 0:
+                print('iteration: ' + str(i) + ' cost: ' + str(current_cost) + ' alpha: ' + str(alpha))
+
             i += 1
-            if i >= n:
+            alpha = (1 / (1 + decay_rate * i)) * initial_alpha  # decrease learning rate
+
+            if i > iteration_num:
                 self.final_theta = theta
                 self.final_cost = costs
                 break
-        plt.plot(range(0,len(costs)),costs)
-        plt.show()  # plot cost
-        return
+
+        # plot the cost trend
+        plt.plot(range(0, len(costs)), costs)
+        plt.xlabel('iteration num')
+        plt.ylabel('cost')
+        plt.show()
+        return self.final_theta, self.final_cost
 
     def predict(self, x, theta):
-        """predict"""
-        p = self.model(x, theta)
+        """
+        predict new data
+        :param x: feature
+        :param theta: weight
+        :return:
+        p: predicted label
+        """
+        p = self.activation(x, theta)
         for i in range(len(p)):
 
-            if p[i] >= 0.5:
+            if p[i] > 0.5:
                 p[i] = 1
             else:
                 p[i] = 0
@@ -89,7 +156,7 @@ class LogisticRegressionModel:
 
 # training
 myModel = LogisticRegressionModel(theta_init)
-myModel.descent(simple_x_train, simple_y_train, 10000, sample_size, 0.03, theta_init)
+myModel.descent(simple_x_train, simple_y_train, 1500, sample_size, 0.07, theta_init, lambd=2, decay_rate=0)
 theta_final = myModel.final_theta
 
 
